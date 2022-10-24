354e0739ad73bd8f0888af74c597769710d4db7c
Fsher07 <furkansalimm@gmail.com>
2022-10-24 19:50:28 +0300

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './app/components/NavBar';
+import Men from './app/pages/Men';
+import Women from './app/pages/Women';
+import Kids from './app/pages/Kids';
 
 function App() {
   return (
     <div className="App">
-      <div>Hello!</div>
+      <NavBar />
+
+      <Routes>
+        <Route path="/women" element={<Women />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/kids" element={<Kids />} />
+      </Routes>
     </div>
   );
 }
--- src/app/components/NavBar/index.jsx
+++ src/app/components/NavBar/index.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import currency from '../../assets/icons/currency.png';
-import cartLogo from '../../assets/icons/empty_cart.svg';
-import middleLogo from '../../assets/icons/VSF.svg';
-import downArrow from '../../assets/icons/down_arrow.svg';
+import currency from '../../../assets/icons/currency.svg';
+import cartLogo from '../../../assets/icons/empty_cart.svg';
+import middleLogo from '../../../assets/icons/VSF.svg';
+import downArrow from '../../../assets/icons/down_arrow.svg';
 import styles from './NavBar.module.css';
 
 class NavBar extends React.PureComponent {
   render() {
     return (
       <header className={styles.navbar}>
-        <nav className="nav-links">
-          <NavLink to="/women" activelassame="active" className="nav-link">
+        <nav className={styles.links}>
+          <NavLink
+            to="/women"
+            activelassame={styles.active}
+            className={styles.link}
+          >
             WOMEN
           </NavLink>
-          <NavLink to="/men" className="nav-link">
+          <NavLink to="/men" className={styles.link}>
             MEN
           </NavLink>
-          <NavLink to="/kids" className="nav-link">
+          <NavLink to="/kids" className={styles.link}>
             KIDS
           </NavLink>
         </nav>
         <img src={middleLogo} alt="middle logo" />
-        <div className="cart-logos">
+        <div className={styles.markings}>
           <img src={currency} alt="currency icon" />
           <img src={downArrow} alt="arrow icon" />
           <img src={cartLogo} alt="empty cart" />
